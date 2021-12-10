3df363b6d3a5f2cd91e456d36c8762628100898e
furkansefademirci <52625217+furkansefademirci@users.noreply.github.com>
2021-12-10 21:51:37 +0300

    Add files via upload

---
+++ sahabul/src/test/java/com/kyb/sahabul/SahabulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyb.sahabul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahabulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
