97fb848f552fb665633f0e910efa23d2c9b01fc7
furkansefademirci <furkansefademirci@gmail.com>
2021-12-23 16:36:56 +0300

    Update PitchPropertyDtoConverter.java
    dto converter metodunu ekledi.

--- sahabul/src/main/java/com/kyb/sahabul/core/converter/PitchPropertyDtoConverter.java
+++ sahabul/src/main/java/com/kyb/sahabul/core/converter/PitchPropertyDtoConverter.java
@@ -1,6 +1,7 @@
 package com.kyb.sahabul.core.converter;
 
 import com.kyb.sahabul.entities.concretes.PitchProperty;
+import com.kyb.sahabul.entities.dto.PitchPropertyDto;
 import com.kyb.sahabul.entities.dto.PitchPropertyForPitchDto;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +9,20 @@ import org.springframework.stereotype.Component;
 public class PitchPropertyDtoConverter {
 
     private final PropertyDtoConverter propertyDtoConverter;
+    private final PitchDtoConverter pitchDtoConverter;
 
-    public PitchPropertyDtoConverter(PropertyDtoConverter propertyDtoConverter) {
+    public PitchPropertyDtoConverter(PropertyDtoConverter propertyDtoConverter, PitchDtoConverter pitchDtoConverter) {
         this.propertyDtoConverter = propertyDtoConverter;
+        this.pitchDtoConverter = pitchDtoConverter;
+    }
+
+    //*
+
+    public PitchPropertyDto convert(PitchProperty from){
+        return new PitchPropertyDto(from.getId(),
+                pitchDtoConverter.convert(from.getPitch()),
+                propertyDtoConverter.convert(from.getProperty())
+                );
     }
 
     public PitchPropertyForPitchDto convertToPitchPropertyForPitchDto(PitchProperty from)
