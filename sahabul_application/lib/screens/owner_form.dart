64134d71af5725c7c06711753a14e36b827c3ee6
s1rat-dev <siratsemih@gmail.com>
2021-12-28 20:55:04 +0300

    Profile ve owner form'u düzenledi.

--- sahabul_application/lib/screens/owner_form.dart
+++ sahabul_application/lib/screens/owner_form.dart
@@ -1,25 +1,143 @@
-
 import 'package:flutter/material.dart';
 import 'package:sahabul_application/components/reusable_widget.dart';
 
-class OwnerForm extends StatelessWidget {
+class OwnerForm extends StatefulWidget {
+  @override
+  State<OwnerForm> createState() => _OwnerFormState();
+}
 
+class _OwnerFormState extends State<OwnerForm> {
+  String? value;
   @override
   Widget build(BuildContext context) {
-    return ReusableWidget(child:
-    Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text('Halı saha adı:',
-        textAlign: TextAlign.left,),
-        TextField(
-          cursorColor: Colors.white,
-          decoration: InputDecoration(
-            hintText: 'enter here',
-
-          ),
-        )
-      ],
-    ));
+    return ReusableWidget(
+        paddingHorizontal: 50,
+        paddingvertical: 30,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                DropdownButton<String>(
+                  value: value,
+                  hint: Text('İl seçin'),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      this.value = newValue;
+                    });
+                  },
+                  items: <String>['One', 'Two', 'Free', 'Four']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                DropdownButton<String>(
+                  value: value,
+                  hint: Text('İlçe Seçin'),
+                  onChanged: value == null
+                      ? null
+                      : (String? value) {
+                          setState(() {
+                            this.value = value;
+                          });
+                        },
+                  items: <String>['One', 'Two', 'Free', 'Four']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Text(
+                    'Halı saha adı:',
+                    textAlign: TextAlign.left,
+                  ),
+                  TextField(
+                    cursorColor: Colors.white,
+                    decoration: InputDecoration(
+                      hintText: 'enter here',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Text(
+                    'Halı saha numarası:',
+                    textAlign: TextAlign.left,
+                  ),
+                  TextField(
+                    cursorColor: Colors.white,
+                    decoration: InputDecoration(
+                      hintText: 'enter here',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Text(
+                    'Halı saha adresi:',
+                    textAlign: TextAlign.left,
+                  ),
+                  TextField(
+                    cursorColor: Colors.white,
+                    decoration: InputDecoration(
+                      hintText: 'enter here',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            TextButton(
+                onPressed: () {
+                  showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return ListView.builder(
+                          padding: EdgeInsets.all(0),
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text('Provider'),
+                              onTap: () {},
+                              trailing: Checkbox(
+                                value: true,
+                                activeColor: Colors.lightBlueAccent,
+                                onChanged: (newValue) {},
+                              ),
+                            );
+                          },
+                          itemCount: 20,
+                        );
+                      });
+                },
+                child: Text('Halı saha özelliklerini seç.'))
+          ],
+        ));
   }
 }
--- sahabul_application/lib/screens/profile.dart
+++ sahabul_application/lib/screens/profile.dart
@@ -8,45 +8,52 @@ class Profile extends StatelessWidget {
     return ReusableWidget(
         paddingHorizontal: 40,
         paddingvertical: 0,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              child: Column(
-                children: [
-                  Icon(
-                    Icons.account_circle_rounded,
-                    size: 120,
-                  ),
-                  //TODO: Style'lar güncellenecek.
-                  Text('Sırat Semih Çöp'),
-                  Text('05061583811'),
-                  Text('siratsemih@gmail.com'),
-                ],
+        child:Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          child: Column(
+            children: [
+              Icon(
+                Icons.account_circle_rounded,
+                size: 120,
               ),
-            ),
-            Container(
-              height: 15,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, 'update_information');
-                    },
-                    child: Text('Bilgilerimi güncelle.'),
-                  ),
-                  GestureDetector(
-                    onTap: (){
-                      Navigator.pushNamed(context, 'owner_form');
-                    },
-                    child: Text('Halı saha sahibi olmak istiyorum.'),
-                  )
-                ],
+              //TODO: Style'lar güncellenecek.
+              Text('Sırat Semih Çöp'),
+              Text('05061583811'),
+              Text('siratsemih@gmail.com'),
+            ],
+          ),
+        ),
+        Container(
+          child: Column(
+            children: [
+              //TODO: Son rezervasyon kartı gelecek. ? yoksa bilgisi yazacak.
+              Card(
+                child: Text('deneme'),
               ),
-            )
-          ],
-        ));
+              BuildBtn(
+                  text: 'Rezervasyonlarım',
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'reservations');
+                  })
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, 'update_information');
+          },
+          child: Text('Bilgilerimi güncelle.'),
+        ),
+        GestureDetector(
+          onTap: (){
+            Navigator.pushNamed(context, 'owner_form');
+          },
+          child: Text('Halı saha sahibi olmak istiyorum.'),
+        )
+      ],
+    ));
   }
 }
